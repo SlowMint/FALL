1e4a70d4daae467528f843c1308b91be1fb3a459
SlowMint <individualmotiv@gmail.com>
2022-10-25 19:41:08 -0500

    Initial Commit

---
+++ FALL/ApexStats.cs
@@ -0,0 +1,23 @@
+﻿namespace FALL
+{
+    public class ApexStats
+    {
+        public ApexStats()
+        {
+
+        }
+
+        public static void DisplayStats()
+        {
+            var client = new HttpClient();
+
+            var apiURL = "https://api.mozambiquehe.re/bridge?auth=304c12dd29a264b48a99695f6e4389e5&player=AlienRift6782&platform=X1";
+
+            var apiDisplay = client.GetStringAsync(apiURL).Result;
+
+            Console.WriteLine(apiDisplay);
+        }
+    }
+
+
+}
